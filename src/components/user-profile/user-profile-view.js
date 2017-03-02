b5d4a371fabcca565c05a84b4e0f84b3cfc93102
Karol Jakubcewicz <karol.jakubcewicz@gmail.com>
2017-03-02 15:42:46 +0100

    FD6SL-109 user profile columns with sample degradation added. Avatar moved to temporary public img folder

--- src/components/user-profile/user-profile-view.js
+++ src/components/user-profile/user-profile-view.js
@@ -6,19 +6,17 @@ const UserProfileView = () =>
     <Grid>
       <h1>Profil Użytkownika</h1>
       <Row>
-        <Col xs={6} md={4}>
-          <Thumbnail src="/assets/thumbnaildiv.png" alt="242x200">
-            <h3>Imię i nazwisko / nazwa użytkownika</h3>
-            <p>Sekcja o mnie</p>
-            <p>
-              <Button bsStyle="primary">Button</Button>&nbsp;
-              <Button bsStyle="default">Button</Button>
-            </p>
+        <Col xs={12} sm={4} md={4}>
+          <Thumbnail className="user--avatar" src={process.env.PUBLIC_URL + '/img/board-games-with-roles.svg'} alt="Avatar użytkownika">
           </Thumbnail>
+        </Col>
+
+        <Col xs={12} sm={8} md={8}>
+          <h3>Imię i nazwisko / nazwa użytkownika</h3>
+            <p>Sekcja o mnie</p>
         </Col>
       </Row>
     </Grid>
-
   )
 
 export default UserProfileView
