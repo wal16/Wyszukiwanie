f45732842ec1f8ea7473cbe888975b8f243ec027
Rafał Szatranski <rafal.szatranski@gmail.com>
2017-03-03 15:45:35 +0100

    GameListView table created

--- src/components/games-list/games-list.js
+++ src/components/games-list/games-list.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {Grid, Table, Button} from 'react-bootstrap'
 
 
-import data from '../../data'
+const GamesListView = ({}) => (
+  <Grid>
+    <h1>Lista Gier</h1>
 
-const GamesListView = () => (
-  <h1>Lista gier</h1>
+    <Table striped>
+      <thead>
+      <tr>
+        <th>Obrazek</th>
+        <th>Nazwa Gry</th>
+        <th>Opis</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+
+      </tbody>
+    </Table>
+  </Grid>
 )
 
 export default GamesListView
