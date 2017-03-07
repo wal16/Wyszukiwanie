79b36c296e6fd370a8e7c1f522daf1d59a7f0e51
Rafał Szatranski <rafal.szatranski@gmail.com>
2017-03-07 15:35:44 +0100

    redux thunk installed

--- src/store.js
+++ src/store.js
@@ -1,15 +1,17 @@
-import {createStore, combineReducers} from 'redux'
+import {createStore, combineReducers, compose, applyMiddleware } from 'redux'
 import gamesReducer from './state/games'
-
-
+import thunk from 'redux-thunk'
 
 const reducer = combineReducers({
   games: gamesReducer
 })
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+);
 
 export default store
