24f50b61edafea2ac209c3284027867b23dee25f
Natalia Kulas <nataliakulas@gmail.com>
2017-03-10 16:29:29 +0100

    add button and link to game list(search) on main page

--- src/components/main-view.js
+++ src/components/main-view.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Jumbotron, Grid, Button} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
 class MainView extends React.Component {
   render() {
@@ -7,9 +8,12 @@ class MainView extends React.Component {
       <Grid>
         <Jumbotron>
           <h1>Masz grę planszową, która&nbsp;się kurzy?</h1>
-          <h2>Aplikacja PożyczME umożliwi Ci&nbsp;
-            wymianę nieużywanych planszówek z&nbsp;innymi graczami!</h2>
-          <p><Button bsStyle="primary">Learn more</Button></p>
+          <h2>Aplikacja PożyczME umożliwi Ci wymianę nieużywanych planszówek z&nbsp;innymi graczami!</h2>
+          <p>
+            <LinkContainer to="/games-list">
+              <Button bsStyle="primary">Przejdź do wyszukiwarki</Button>
+            </LinkContainer>
+          </p>
         </Jumbotron>
       </Grid>
 
