fb90aaa71b869db752982bc328aab1f1bec354bc
Natalia Kulas <nataliakulas@gmail.com>
2017-03-15 16:56:58 +0100

    FD6SL-166 slider somewhere in between

--- src/components/game-ranges/game-ranges.js
+++ src/components/game-ranges/game-ranges.js
@@ -2,15 +2,19 @@ import React from 'react'
 import {connect} from 'react-redux'
 import InputRange from 'react-input-range'
 
-import { slider } from '../../state/range'
+import { sliderMin } from '../../state/range'
+import { sliderMax } from '../../state/range'
+
 
 export default connect(
   state => ({
     games: state.games,
-    gameRange: state.slider
+    minValue: state.sliderMin.minValue,
+    maxValue: state.sliderMax.maxValue
   }),
   dispatch => ({
-    slider: (value) => dispatch(slider(value))
+    sliderMin: (value) => dispatch(sliderMin(value)),
+    sliderMax: (value) => dispatch(sliderMax(value))
   })
 )(
   class GameRanges extends React.Component {
@@ -18,15 +22,17 @@ export default connect(
       super(props);
 
       this.state = {
-        value: { min: 2, max: 10 },
-      };
+        minValue,
+        maxValue
+      }
+
     }
 
     render() {
       return (
         <InputRange
-          maxValue={20}
-          minValue={0}
+          maxValue={maxValue}
+          minValue={minValue}
           value={this.state.value}
           onChange={value => this.setState({ value })} />
       );
--- src/state/range.js
+++ src/state/range.js
@@ -1,20 +1,32 @@
-const RANGE = 'game-list/RANGE'
+const CHANGE_MIN = 'game-list/CHANGE_MIN'
+const CHANGE_MAX = 'game-list/CHANGE_MAX'
 
-export const slider = (value) => ({
-  type: RANGE,
+export const sliderMin = (value) => ({
+  type: CHANGE_MIN,
+  value
+})
+
+export const sliderMax = (value) => ({
+  type: CHANGE_MAX,
   value
 })
 
 const initialState = {
-  gameRange: 4
+  minValue: 2,
+  maxValue: 20
 }
 
 export default (state = initialState, action = {}) => {
   switch (action.type) {
-    case RANGE:
+    case CHANGE_MIN:
       return {
         ...state,
-        gameRange: action.value
+        minValue: action.value
+      }
+    case CHANGE_MAX:
+      return {
+        ...state,
+        maxValue: action.value
       }
     default:
       return state
