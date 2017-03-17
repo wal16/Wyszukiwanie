faa68a29fa55fa7c4c9dbbc5e9b062acb252cef0
Marta Rzadkowska <marta.rzadkowska@gmail.com>
2017-03-17 16:06:10 +0100

    FD6SL-169 push

--- src/components/games-list-view/games-list-view.js
+++ src/components/games-list-view/games-list-view.js
@@ -118,6 +118,7 @@ export default connect(
                   Nie znaleziono gier spełniających kryteria wyszukiwania. Spróbuj wyszukać inny tytuł...
                 </Alert>
               )
+
           }
         </Grid>
       )
