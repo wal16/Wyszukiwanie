183e160c70aea520c9b214c91f7ca8cba0391eed
Karol Jakubcewicz <karol.jakubcewicz@gmail.com>
2017-03-22 11:36:34 +0100

    FD6SL-179 user data read from store. Deleted obsolete state connections

--- src/components/my-profile-view/my-profile-view.js
+++ src/components/my-profile-view/my-profile-view.js
@@ -6,21 +6,17 @@ import {Tabs, Tab} from 'react-bootstrap-tabs'
 
 export default connect(
   state => ({
-    users: state.users,
-    games: state.games,
     user: state.user
   })
 )(
   class MyProfileView extends React.Component {
     render() {
       const {
-        users,
-        games,
         user
       } = this.props
 
       const currentUser =
-        users.data ? users.data[4] : null
+        user.data ? user.data : null
 
       if (currentUser === null) {
         return <p>Waiting for users data...</p>
@@ -33,75 +29,15 @@ export default connect(
           <Row>
             <Col xs={12} md={4} sm={6}>
               <div key={currentUser.id}>
-                <Image
-                  src={process.env.PUBLIC_URL + currentUser.picture}
-                  alt={"Awatar użytkownika " + currentUser.name}
-                  responsive
-                />
+
               </div>
             </Col>
 
             <Col xs={12} sm={6} md={8}>
               <div key={currentUser.id}>
-                <h3>{currentUser.name} {currentUser.surname}</h3>
-                <p>{currentUser.about}</p>
+                <h3>{currentUser.username}</h3>
+                <p>About</p>
               </div>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12} md={12}>
-              <Panel>
-                <Tabs id="noanim-tab-example">
-                  <Tab headerClass='pointer' label="Posiadam">
-                    <h3>Posiadam</h3>
-                    <ul>
-                      {
-                        currentUser.id ?
-                          <ul>
-                            {
-                              currentUser.gameList.map(
-                                game => games.data.find(g => g.id === game)
-                              ).map(
-                                game => (
-                                  <li key={game.id}>
-                                    <Link to={'game-profile/' + game.id}>
-                                      {game.name}
-                                    </Link>
-                                  </li>
-                                )
-                              )
-                            }
-                          </ul> : null
-                      }
-
-                    </ul>
-                  </Tab>
-                  <Tab headerClass='pointer' label="Szukam">
-                    <h3>Szukam</h3>
-                    <ul>
-                      {
-                        currentUser.id ?
-                          <ul>
-                            {
-                              currentUser.wishList.map(
-                                game => games.data.find(g => g.id === game)
-                              ).map(
-                                game => (
-                                  <li key={game.id}>
-                                    <Link to={'game-profile/' + game.id}>
-                                      {game.name}
-                                    </Link>
-                                  </li>
-                                )
-                              )
-                            }
-                          </ul> : null
-                      }
-
-                    </ul>
-                  </Tab>
-                </Tabs>
-              </Panel>
             </Col>
           </Row>
         </Grid>
