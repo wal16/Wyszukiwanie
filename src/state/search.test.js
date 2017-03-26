f0046c30bff4eeefd133096cc701396bc5b7f9d5
Karol Jakubcewicz <karol.jakubcewicz@gmail.com>
2017-03-26 22:06:18 +0200

    FD6SL-186 additional search reducer tests added

--- src/state/search.test.js
+++ src/state/search.test.js
@@ -18,4 +18,44 @@ describe('Search reducer', () => {
       searchString: 'foo'
     })
   })
+
+  it('should handle uppercase characters', () => {
+    expect(
+      reducer(
+        {searchString: ''}, search('Foo Bar')
+      )
+    ).toEqual({
+      searchString: 'Foo Bar'
+    })
+  })
+
+  it('should handle digits', () => {
+    expect(
+      reducer(
+        {searchString: ''}, search('36')
+      )
+    ).toEqual({
+      searchString: '36'
+    })
+  })
+
+  it('should handle html as strings', () => {
+    expect(
+      reducer(
+        {searchString: ''}, search('<footer>')
+      )
+    ).toEqual({
+      searchString: '<footer>'
+    })
+  })
+
+  it('should handle javascript as strings', () => {
+    expect(
+      reducer(
+        {searchString: ''}, search("document.getElementsByClassName('Foo').append(div.bar)")
+      )
+    ).toEqual({
+      searchString: "document.getElementsByClassName('Foo').append(div.bar)"
+    })
+  })
 })
