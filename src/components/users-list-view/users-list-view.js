6fbde9328aaff40f1505c09b2092462d8634ebba
Natalia Kulas <nataliakulas@gmail.com>
2017-03-26 13:34:25 +0200

    FD6SL-184 users list view done

--- src/components/users-list-view/users-list-view.js
+++ src/components/users-list-view/users-list-view.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router'
-import {Grid, Table} from 'react-bootstrap'
+import {Grid, Table, Alert, Panel, Row, Col, Image, Button, Glyphicon} from 'react-bootstrap'
 
+import './users-list-view.css'
 
 export default connect(
   state => ({
@@ -11,86 +12,94 @@ export default connect(
   }),
   dispatch => ({})
 )(
+  class UsersListView extends React.Component {
+    render() {
+      const {
+        users,
+        games,
+      } = this.props
 
-class UsersListView extends React.Component {
-  render() {
-    const {
-      users,
-      games,
-    } = this.props
-
-    return (
-      <Grid>
-        <h1>Lista użytkowników</h1>
-        <Table striped>
-          <thead>
-          <tr>
-            <th></th>
-            <th>Imię i nazwisko</th>
-            <th>Posiadane gry</th>
-            <th>Lista życzeń</th>
-          </tr>
-          </thead>
-          <tbody>
-          {
-            users.data ?
-              users.data.map(
-                user => (
-                  <tr key={user.id}>
-                    <td>
-                      <img src={user.picture}
-                             alt="Zdjecie gry"
-                             height="70"
-                      />
-                    </td>
-                    <td>
-                      <Link to={'/user-profile/' + user.id}>
-                        {user.name} {user.surname}
-                      </Link>
-                    </td>
-                    <td>
-                      {
-                        user.id ? (
-                              user.gameList.map(
-                                gameId => games.data.find(game => game.id === gameId)
-                              ).map(
-                                game => (
-                                  <div key={game.id}>
-                                    <Link to={'game-profile/' + game.id}>
-                                      {game.name}
-                                    </Link>
-                                  </div>
-                                )
-                              )
-                        ) : null
-                      }
-                    </td>
-                    <td>
-                      {
-                        user.id ? (
-                              user.wishList.map(
-                                game => games.data.find(g => g.id === game)
-                              ).map(
-                                game => (
-                                  <div key={game.id}>
-                                    <Link to={'game-profile/' + game.id}>
-                                      {game.name}
-                                    </Link>
-                                  </div>
-                                )
-                              )
-                        ) : null
-                      }
-                    </td>
-                  </tr>
-                )
-              )
-              : <tr><td colSpan="4">Oczekiwanie na dane użytkowników...</td></tr>
-          }
-          </tbody>
-        </Table>
-      </Grid>
-    )
+      return (
+        <Grid>
+          <Panel header="Użytkownicy"
+                 className="panel-body__users-list">
+            <div className="panel-body-table__users-list">
+              <Table className="table__users-list table-hover">
+                <thead className="table-head__users-list">
+                <tr className="table-tr__users-list">
+                  <th className="table__users-list-empty"/>
+                  <th className="table-th__users-list">Imię i nazwisko</th>
+                  <th className="table-th__users-list table__users-list-empty">Posiadane gry</th>
+                  <th className="table-th__users-list table__users-list-empty">Lista życzeń</th>
+                </tr>
+                </thead>
+                <tbody className="table-body__users-list">
+                {
+                  users.data ?
+                    users.data.map(
+                      user => (
+                        <tr className="table-tr__users-list"
+                            key={user.id}>
+                          <td className="table-td__users-list table__users-list-empty">
+                            <div className="user-image__wrapper">
+                              <Image className="user-image__users-list"
+                                     src={user.picture}
+                                     alt="Zdjecie gry"
+                              />
+                            </div>
+                          </td>
+                          <td className="table-td__users-list">
+                            <Link to={'/user-profile/' + user.id}>
+                              {user.name} {user.surname}
+                            </Link>
+                          </td>
+                          <td className="table-td__users-list table__users-list-empty">
+                            {
+                              user.id ? (
+                                  user.gameList.map(
+                                    gameId => games.data.find(game => game.id === gameId)
+                                  ).map(
+                                    game => (
+                                      <div key={game.id}>
+                                        <Link to={'game-profile/' + game.id}>
+                                          {game.name}
+                                        </Link>
+                                      </div>
+                                    )
+                                  )
+                                ) : null
+                            }
+                          </td>
+                          <td className="table-td__users-list table__users-list-empty">
+                            {
+                              user.id ? (
+                                  user.wishList.map(
+                                    game => games.data.find(g => g.id === game)
+                                  ).map(
+                                    game => (
+                                      <div key={game.id}>
+                                        <Link to={'game-profile/' + game.id}>
+                                          {game.name}
+                                        </Link>
+                                      </div>
+                                    )
+                                  )
+                                ) : null
+                            }
+                          </td>
+                        </tr>
+                      )
+                    )
+                    : <tr>
+                      <td colSpan="4">Oczekiwanie na dane użytkowników...</td>
+                    </tr>
+                }
+                </tbody>
+              </Table>
+            </div>
+          </Panel>
+        </Grid>
+      )
+    }
   }
-}
 )
