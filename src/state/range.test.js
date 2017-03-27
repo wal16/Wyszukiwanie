704ef55e8e57e0c8f7ba5f8e044f982f46975577
Karol Jakubcewicz <karol.jakubcewicz@gmail.com>
2017-03-27 13:45:12 +0200

    FD6SL-186 reset and range action test

--- src/state/range.test.js
+++ src/state/range.test.js
@@ -1,4 +1,4 @@
-import reducer, {minLabel, maxLabel} from './range'
+import reducer, {range, reset, minLabel, maxLabel} from './range'
 
 describe('Range reducer', () => {
   it('should return initial state', () => {
@@ -8,6 +8,32 @@ describe('Range reducer', () => {
       minLabel: minLabel,
       maxLabel: maxLabel,
       changeRange: {min: minLabel, max: maxLabel}
+    })
+  })
+
+  it('reset action should return initial state', () => {
+    expect(
+      reducer(
+        {
+          changeRange: {min: 10, max: 18}
+        }, reset()
+      )
+    ).toEqual({
+      minLabel: minLabel,
+      maxLabel: maxLabel,
+      changeRange: {min: minLabel, max: maxLabel}
+    })
+  })
+
+  it('should handle changeRange action', () => {
+    expect(
+      reducer(
+        {
+          changeRange: {min: minLabel, max: maxLabel}
+        }, range({min: 2, max: 8})
+      )
+    ).toEqual({
+      changeRange: {min: 2, max: 8}
     })
   })
 })
