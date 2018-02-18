bfb45f86718edfcee8f4e8ad69ff1fd94008721c
waldemar <waldemar.mozola@onet.pl>
2018-02-18 20:59:57 +0100

    add

--- pozyczME/src/test/java/pageobjects/tests/SearchFor.java
+++ pozyczME/src/test/java/pageobjects/tests/SearchFor.java
@@ -2,8 +2,12 @@ package pageobjects.tests;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SearchFor {
 
@@ -14,11 +18,23 @@ public class SearchFor {
     public void setUp() {
 
 
-        System.setProperty("webdriver.chrome.driver", "src/test/java/resources/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
         driver = new ChromeDriver();
     }
 
+    @Test
+        public void addToSearch(){
+        driver.get("http://app.pinapple.jdqz1.is-academy.pl/games-list");
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        WebElement pageSearch = driver.findElement(By.xpath("//*[@id=\"search\"]"));
+    }
+
+
+
 
     @After
-    public void tearDown() { driver.close(); }
+
+    public void tearDown() {
+        driver.close();
+    }
 }
